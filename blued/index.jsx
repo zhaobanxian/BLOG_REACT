587f6b35567629599b051350259936f864484497
zhaobanxian <zhaoshiping198511@163.com>
2018-11-05 19:54:23 +0800

    blued首屏处理闪屏未果

--- blued/index.jsx
+++ blued/index.jsx
@@ -1,18 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Screen_1 from './screen_1.jsx';
+import Screen_2 from './screen_2.jsx';
 import './index.css'
+import { Carousel } from 'antd-mobile'
 
-window.onload=()=>{
-    ReactDOM.render(
-        <div className="scroll_content">
-            <div className="scroll">
-                <Screen_1></Screen_1>
+class Index extends React.Component {
+    constructor(props) {
+        super(props);
+        this.selectedIndex = 0;
+    }
+    componentDidMount() {
+    }
+    beforeChange(from, to) {
+        if (this.selectedIndex != to) {
+            this.selectedIndex = to;
+            console.log(to);
+            this.setState({});
+        }
+    }
+    render() {
+        return <div className="scroll_content">
+            <div className="scroll" ref='scbox'>
+                <Carousel vertical={true} dots={false} selectedIndex={this.selectedIndex} beforeChange={this.beforeChange.bind(this)}>
+                    <Screen_1 iscurrent={this.selectedIndex == 0}></Screen_1>
+                    <Screen_2 iscurrent={this.selectedIndex == 1}></Screen_2>
+                </Carousel>
             </div>
             <div className='updown'>
                 <img src='https://dn-web-blued-cn.qbox.me/web/static/liveactivity/arrow-1b5f115f.png'></img>
             </div>
-        </div>,
+        </div>
+    }
+}
+window.onload = () => {
+    ReactDOM.render(
+        <Index></Index>,
         document.getElementById("content")
     )
 }
--- blued/screen_1.jsx
+++ blued/screen_1.jsx
@@ -1,20 +1,49 @@
 
 import React from 'react';
-import('./screen_1.css');
+import './screen_1.css';
 
 export default class Screen_1 extends React.Component {
     constructor(props) {
-        this.load();
         super(props);
     }
-    load(){
-        
+    componentDidMount() {
+    }
+    componentWillReceiveProps(nextprops) {
+        if (this.props.iscurrent != nextprops.iscurrent) {
+            this.setimg1action();
+            this.setimg2action();
+            this.setState({});
+        }
+    }
+    setimg1action() {
+        if (this.props.iscurrent) {
+            this.img1actionstyle = {
+                animation: 'img1_action 1.5s'
+            }
+        } else {
+            this.img1actionstyle = {
+                transform: "translateY(-200%)"
+            }
+        }
+        this.setState({});
+    }
+    setimg2action() {
+        if (this.props.iscurrent) {
+            this.img2actionstyle = {
+                animation: "img2_action 1.5s"
+            }
+        } else {
+            this.img2actionstyle = {
+                transform: "translateY(200%)"
+            }
+        }
+        this.setState({});
     }
     render() {
         return <div className="screenmain screenmain1">
             <div className='lan'>EN</div>
-            <img className="img1" src="https://os4ty6tab.qnssl.com/cblued/static/m_index_logo.1ccsg2q112jiqn.png"></img>
-            <img className="img2" src="https://dn-web-blued-cn.qbox.me/web/static/liveactivity/phone-fea27fab.png"></img>
+            <img className="img1" style={this.img1actionstyle} onLoad={this.setimg1action.bind(this)} src="https://os4ty6tab.qnssl.com/cblued/static/m_index_logo.1ccsg2q112jiqn.png"></img>
+            <img className='img2' style={this.img2actionstyle} onLoad={this.setimg2action.bind(this)} src="https://dn-web-blued-cn.qbox.me/web/static/liveactivity/phone-fea27fab.png"></img>
             <div className="dibu_mnue">
                 <ul className="set_ul">
                     <li><a>立即下载</a></li>
