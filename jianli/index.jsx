d062941d7ebf23f27a1adb6dbfca1b5c74bcdb0b
zhaobanxian <zhaoshiping198511@163.com>
2018-11-09 19:25:44 +0800

    ss

---
+++ jianli/index.jsx
@@ -0,0 +1,12 @@
+import react from 'react';
+import reactDOM from 'react-dom';
+
+/* */
+class JianLi extends react.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return <div></div>
+    }
+}
